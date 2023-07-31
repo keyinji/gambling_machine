a522a231ff8c177d10a1be76bf688bf3edd6126e
keyinji <92763672+keyinji@users.noreply.github.com>
2023-07-30 21:30:02 -0400

    done for today

--- main.py
+++ main.py
@@ -42,10 +42,10 @@ def print_slot_machine(columns):
     for row in range(len(columns[0])):
         for i, column in enumerate(columns):
             if i != len(columns) - 1:
-                print(column[row], "|")
+                print(column[row],end= " | ")
             else:
-                print(column[row])
-
+                print(column[row], end="")
+        print()
 
 
 def deposit():
@@ -105,6 +105,9 @@ def main():
             break
 
     print(f"You are betting ${bet} on {lines}. Total bet: ${total}")
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 
 main()
